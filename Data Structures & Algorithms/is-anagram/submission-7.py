f06e2041988dad8472985b220505b1b09d72d7f9
Mukesh Royal <Mukeshroyal455@gmail.com>
2026-09-12 11:25:30 -0400

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        
+        temp1 = {}
+        temp2 = {}
+        for i in s:
+            if i not in temp1:
+                temp1[i] = 1
+            else:
+                temp1[i] += 1
+        for i in t:
+            if i not in temp2:
+                temp2[i] = 1
+            else:
+                temp2[i] += 1
+        return temp1 == temp2
+        
+
